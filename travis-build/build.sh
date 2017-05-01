d25fb334043bf80f1dcfc29d54ef28a951d63285
Alexis Murzeau <amubtdx@outlook.fr>
2017-05-01 19:45:43 +0200

    deboostrap fail to find dependencies of sbuild, install additional dependencies later

--- travis-build/build.sh
+++ travis-build/build.sh
@@ -6,7 +6,7 @@ CHROOT_DIST=unstable
 CHROOT_ARCH=amd64
 CHROOT_NAME=$CHROOT_DIST-$CHROOT_ARCH-sbuild
 CHROOT_DEBIAN_MIRROR=http://ftp.de.debian.org/debian/
-CHROOT_ADDITIONAL_PACKETS=gnupg,lintian,sbuild,schroot
+CHROOT_ADDITIONAL_PACKETS="gnupg lintian sbuild schroot"
 
 # Install dependencies on ubuntu to create a chroot with debian unstable
 sudo apt-get install -y --no-install-recommends git-buildpackage dpkg-dev schroot sbuild debootstrap
@@ -16,8 +16,7 @@ git-buildpackage --git-verbose --git-ignore-branch
 
 # Create debian unstable chroot
 mkdir ~/chroot
-sudo sbuild-createchroot --arch=$CHROOT_ARCH $CHROOT_DIST ~/chroot/$CHROOT_NAME/ $CHROOT_DEBIAN_MIRROR --keyring= --include=$CHROOT_ADDITIONAL_PACKETS ||
-(cat ~/chroot/$CHROOT_NAME/debootstrap/debootstrap.log && exit 2)
+sudo sbuild-createchroot --arch=$CHROOT_ARCH $CHROOT_DIST ~/chroot/$CHROOT_NAME/ $CHROOT_DEBIAN_MIRROR --keyring= || (cat ~/chroot/$CHROOT_NAME/debootstrap/debootstrap.log && exit 2)
 
 # Configure schroot
 sudo bach -c "echo 'union-type=overlayfs' >> /etc/schroot/chroot.d/$CHROOT_NAME*"
@@ -33,6 +32,8 @@ sudo mkdir -p ~/chroot/$CHROOT_NAME/etc/schroot/chroot.d/
 sudo bash -c "echo '/home/$USER  /home/$USER none  rw,bind 0       0' >> ~/chroot/$CHROOT_NAME/etc/schroot/sbuild/fstab"
 sudo cp /etc/schroot/chroot.d/$CHROOT_NAME* ~/chroot/$CHROOT_NAME/etc/schroot/chroot.d/
 sudo sed -i 's/script-config=.*/profile=sbuild/' ~/chroot/$CHROOT_NAME/etc/schroot/chroot.d/$CHROOT_NAME*
+
+schroot -c "$CHROOT_NAME" -u root -- apt-get install -y -no-install-recommends $CHROOT_ADDITIONAL_PACKETS
 
 SESSION=$(schroot --begin-session -c $CHROOT_NAME)
 echo $SESSION
