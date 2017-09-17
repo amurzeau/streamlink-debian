45b3a3987a0d1c4d2003c1307437b41c02878446
Alexis Murzeau <amubtdx@outlook.fr>
2017-09-17 18:43:12 +0200

    travis: fix sbuild with ubuntu trusty

--- travis-build/build.sh
+++ travis-build/build.sh
@@ -3,6 +3,8 @@
 set -xe
 
 CHROOT_DIST=${1:-stable}
+CHROOT_ALIAS=$2
+CHROOT_TARGET_DIST=${CHROOT_ALIAS:-$CHROOT_DIST}
 CHROOT_ARCH=amd64
 CHROOT_NAME=$CHROOT_DIST-$CHROOT_ARCH-sbuild
 CHROOT_DEBIAN_MIRROR=http://ftp.de.debian.org/debian/
@@ -18,27 +20,36 @@ git-buildpackage --git-verbose --git-ignore-branch '--git-builder=dpkg-source -b
 id -u _apt > /dev/null 2>&1 || sudo adduser --force-badname --system --home /nonexistent --no-create-home --quiet _apt || true
 
 # Create debian chroot
-mkdir ~/chroot
-sudo sbuild-createchroot --arch=$CHROOT_ARCH $CHROOT_DIST ~/chroot/$CHROOT_NAME/ $CHROOT_DEBIAN_MIRROR --keyring= || (cat ~/chroot/$CHROOT_NAME/debootstrap/debootstrap.log && exit 2)
+mkdir -p ~/chroot
+if [ ! -d ~/chroot/$CHROOT_NAME ]; then
+	sudo sbuild-createchroot --arch=$CHROOT_ARCH $CHROOT_DIST ~/chroot/$CHROOT_NAME/ $CHROOT_DEBIAN_MIRROR --keyring= || (cat ~/chroot/$CHROOT_NAME/debootstrap/debootstrap.log && exit 2)
 
-# Fix _apt permissions
-sudo chown -R _apt:root ~/chroot/$CHROOT_NAME/var/lib/apt/lists/partial || true
+	# Fix _apt permissions
+	sudo chown -R _apt:root ~/chroot/$CHROOT_NAME/var/lib/apt/lists/partial || true
 
-# Configure schroot
-sudo bash -c "echo 'union-type=overlayfs' >> /etc/schroot/chroot.d/$CHROOT_NAME*"
-cat /etc/schroot/chroot.d/$CHROOT_NAME*
-sudo cp travis-build/sbuild-key.* /var/lib/sbuild/apt-keys/
-sudo bash -c "echo '/home/$USER  /home/$USER none  rw,bind 0       0' >> /etc/schroot/sbuild/fstab"
-sudo bash -c "echo '/var/lib/schroot /var/lib/schroot none  rw,bind 0       0' >> /etc/schroot/sbuild/fstab"
-sudo schroot -c "source:${CHROOT_NAME}" -u root -d / -- apt-get install -y --no-install-recommends $CHROOT_ADDITIONAL_PACKETS
+	# Configure schroot
+	if [ -n "$CHROOT_ALIAS" ]; then
+		sudo bash -c "echo 'aliases=$CHROOT_ALIAS' >> /etc/schroot/chroot.d/$CHROOT_NAME*"
+	fi
+	sudo bash -c "echo 'union-type=overlayfs' >> /etc/schroot/chroot.d/$CHROOT_NAME*"
+	sudo cp travis-build/sbuild-key.* /var/lib/sbuild/apt-keys/
+	sudo bash -c "echo '/home/$USER  /home/$USER none  rw,bind 0       0' >> /etc/schroot/sbuild/fstab"
+	sudo bash -c "echo '/var/lib/schroot /var/lib/schroot none  rw,bind 0       0' >> /etc/schroot/sbuild/fstab"
+	sudo schroot -c "source:${CHROOT_NAME}" -u root -d / -- apt-get install -y --no-install-recommends $CHROOT_ADDITIONAL_PACKETS
 
-# Configure mounts inside schroot
-sudo mkdir -p ~/chroot/$CHROOT_NAME/etc/schroot/chroot.d/
-sudo bash -c "echo '/home/$USER  /home/$USER none  rw,bind 0       0' >> ~/chroot/$CHROOT_NAME/etc/schroot/sbuild/fstab"
-sudo cp /etc/schroot/chroot.d/$CHROOT_NAME* ~/chroot/$CHROOT_NAME/etc/schroot/chroot.d/
-sudo sed -i 's/script-config=.*/profile=sbuild/' ~/chroot/$CHROOT_NAME/etc/schroot/chroot.d/$CHROOT_NAME*
+	# Configure mounts inside schroot
+	sudo mkdir -p ~/chroot/$CHROOT_NAME/etc/schroot/chroot.d/
+	sudo bash -c "echo '/home/$USER  /home/$USER none  rw,bind 0       0' >> ~/chroot/$CHROOT_NAME/etc/schroot/sbuild/fstab"
+	sudo cp /etc/schroot/chroot.d/$CHROOT_NAME* ~/chroot/$CHROOT_NAME/etc/schroot/chroot.d/
+
+	# overlayfs is replaced by overlay in sid's sbuild when having a kernel newer than 3.18 (overlayfs need workdir which is provided only when using overlay instead of overlayfs)
+	sudo sed -i 's/union-type=overlayfs/union-type=overlay/' ~/chroot/$CHROOT_NAME/etc/schroot/chroot.d/$CHROOT_NAME*
+
+	cat /etc/schroot/chroot.d/$CHROOT_NAME*
+	cat ~/chroot/$CHROOT_NAME/etc/schroot/chroot.d/$CHROOT_NAME*
+fi
 
 # Add current user to sbuild group (required by sbuild)
 sudo sbuild-adduser $USER
 
-sudo schroot -c "$CHROOT_NAME" -u $USER -- sbuild -v -As --force-orig-source ../*.dsc -d $CHROOT_DIST --run-lintian --run-autopkgtest --autopkgtest-root-args= --autopkgtest-opts="-- schroot %r-%a-sbuild"
+sudo schroot -c "$CHROOT_NAME" -u $USER -- sbuild -v -As --force-orig-source ../*.dsc -d $CHROOT_DIST --run-lintian --lintian-opts="-EviIL +pedantic" --run-autopkgtest --autopkgtest-root-args= --autopkgtest-opts="-- schroot %r-%a-sbuild"
--- travis-build/deploy.sh
+++ travis-build/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 bintray_repository="${1:-debian}"
-CHROOT_DIST=${1:-stable}
+CHROOT_DIST=${2:-stable}
+CHROOT_ALIAS=$3
+CHROOT_TARGET_DIST=${CHROOT_ALIAS:-$CHROOT_DIST}
 CHROOT_ARCH=amd64
 CHROOT_NAME=$CHROOT_DIST-$CHROOT_ARCH-sbuild
 
@@ -9,5 +11,5 @@ echo "Deploying on bintray repository $bintray_repository"
 
 CHROOT_SESSION=$(sudo schroot -c "${CHROOT_NAME}" --begin-session)
 sudo schroot --run-session -c $CHROOT_SESSION -u root -- apt-get install -y --no-install-recommends python3 python3-debian python3-requests
-sudo -E schroot -p --run-session -c $CHROOT_SESSION -u $USER -- python3 $(dirname "$0")/bintray_upload_changes.py amurzeau $bintray_repository $CHROOT_DIST *.changes
+sudo -E schroot -p --run-session -c $CHROOT_SESSION -u $USER -- python3 $(dirname "$0")/bintray_upload_changes.py amurzeau $bintray_repository $CHROOT_TARGET_DIST *.changes
 sudo schroot --end-session -c $CHROOT_SESSION
