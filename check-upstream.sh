f0e2049ac68e7427c3bdd71649bd5724f2346e8b
Alexis Murzeau <amubtdx@gmail.com>
2018-01-04 00:56:40 +0100

    Use version 3 as ubuntu on travis has a too old uscan

--- check-upstream.sh
+++ check-upstream.sh
@@ -6,4 +6,6 @@ mkdir -p debian
 
 wget -q https://raw.githubusercontent.com/amurzeau/streamlink-debian/master/debian/changelog -O debian/changelog &&
 wget -q https://raw.githubusercontent.com/amurzeau/streamlink-debian/master/debian/watch -O debian/watch &&
+# uscan on travis does not support version 4
+sed -i 's/version=4/version=3/' debian/watch &&
 ! uscan --report-status
