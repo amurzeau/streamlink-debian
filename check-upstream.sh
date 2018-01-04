8a6382ad6ad193769df908ac4d5a23861fc47618
Alexis Murzeau <amubtdx@gmail.com>
2018-01-04 01:00:36 +0100

    Retrieve signing-key.asc needed by uscan

--- check-upstream.sh
+++ check-upstream.sh
@@ -2,10 +2,11 @@
 
 set -x
 
-mkdir -p debian
+mkdir -p debian debian/upstream
 
 wget -q https://raw.githubusercontent.com/amurzeau/streamlink-debian/master/debian/changelog -O debian/changelog &&
 wget -q https://raw.githubusercontent.com/amurzeau/streamlink-debian/master/debian/watch -O debian/watch &&
+wget -q https://raw.githubusercontent.com/amurzeau/streamlink-debian/master/debian/upstream/signing-key.asc -O debian/upstream/signing-key.asc &&
 # uscan on travis does not support version 4
 sed -i 's/version=4/version=3/' debian/watch &&
 ! uscan --report-status
