c8929f85e8d9a7395e1d626bc7b0bc603a9d2ccf
Alexis Murzeau <amubtdx@gmail.com>
2021-09-21 21:34:13 +0200

    d/tests/check_docs: fix looking for doc-base files by using install-docs
    Instead of manually using the doc-base file directly.
    This doc-file name has changed since
    https://salsa.debian.org/debian/debhelper/-/commit/8eac421c260e62bcecd571af225438e107b33157#92bb5c860e88dfd329457a7d81bfa49525d3d18f_451_428
    
    To avoid issues with duplicated document IDs, we will always
    install to usr/share/doc-base/<packagename>.<doc_id> instead
    of just usr/share/doc-base/<packagename> or just
    usr/share/doc-base/<doc_id>. See #525821 and #980903.

--- debian/tests/check_docs.sh
+++ debian/tests/check_docs.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+set -e
+
 RESULT=0
+
+DOCBASE_DATA=$(/usr/sbin/install-docs -s python3-streamlink-doc)
 
 check-file() {
 	local TARGET_FILE="$1"
@@ -14,17 +18,17 @@ check-file() {
 	return 0
 }
 
-if ! [ -f /usr/share/doc-base/python3-streamlink-doc ]; then
-	echo "Cannot read /usr/share/doc-base/python3-streamlink-doc" >&2
+if [ -z "$DOCBASE_DATA" ]; then
+	echo "Cannot read install-docs -s python3-streamlink-doc" >&2
 	exit 1
 fi
 
 echo "Doc-base file content:"
-cat /usr/share/doc-base/python3-streamlink-doc
+echo "$DOCBASE_DATA"
 echo "---------"
 
 echo "Checking Files field"
-DOC_FILES_WILDCARD=$(grep Files /usr/share/doc-base/python3-streamlink-doc | sed 's/Files: //')
+DOC_FILES_WILDCARD=$(grep Files <<<"$DOCBASE_DATA" | sed 's/Files: //')
 echo "Content of $DOC_FILES_WILDCARD:"
 # no quotes intended: doc-base file use wildcards and we want them to be expanded
 # shellcheck disable=SC2086
@@ -39,7 +43,7 @@ if ! [ -f "${DOC_FILES_COUNT[0]}" ]; then
 fi
 
 echo "Checking files:"
-INDEX_HTML_LOCATION=$(grep Index /usr/share/doc-base/python3-streamlink-doc | sed 's/Index: //')
+INDEX_HTML_LOCATION=$(grep Index <<<"$DOCBASE_DATA" | sed 's/Index: //')
 
 case $INDEX_HTML_LOCATION in /usr/share/doc/*)
 	;;
@@ -47,8 +51,6 @@ case $INDEX_HTML_LOCATION in /usr/share/doc/*)
 	echo "index.html must be in /usr/share/doc/ but is \"$INDEX_HTML_LOCATION\"" >&2
 	exit 2
 esac
-
-HTML_BASE_DIR=$(dirname "$INDEX_HTML_LOCATION")
 
 check-file "$INDEX_HTML_LOCATION" || exit 3
 
