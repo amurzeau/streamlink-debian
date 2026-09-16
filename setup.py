1b5606d88f185b687e30cd7f340c2b4f08170a8f
Alexis Murzeau <amubtdx@gmail.com>
2026-09-16 20:36:13 +0200

    New upstream version 8.6.1

--- setup.py
+++ setup.py
@@ -91,5 +91,5 @@ if __name__ == "__main__":
         cmdclass=get_cmdclasses(cmdclass),
         entry_points=entry_points,
         data_files=data_files,
-        version="8.6.0",
+        version="8.6.1",
     )
